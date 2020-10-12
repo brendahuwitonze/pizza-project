1cd5872ac8100fc165deb513971d961cf6a6cdb9
uwitonze brendah <uwitonzebrendah@gmail.com>
2020-10-12 13:40:14 +0200

    made changes in css

--- js/script.js
+++ js/script.js
@@ -1,115 +1,134 @@
 
-$(document).ready(function () {
-    $('.dore').mouseover(function () {
-         $('.epi').show();
-         $('.lil').show();
-    })
+// $(document).ready(function () {
+//     $('.dore').mouseover(function () {
+//         $('.epi').show();
+//         $('.lil').show();
+//     })
 
-    $('.dore').mouseout(function () {
-         $('.epi').hide();
-         $('.lil').hide();
-    })
-});
+//     $('.dore').mouseout(function () {
+//         $('.epi').hide();
+//         $('.lil').hide();
+//     })
+// });
 
-$(document).ready(function () {
-    $('.josh').mouseover(function () {
-         $('.mug').show();
-         $('.kim').show();
-    })
+// $(document).ready(function () {
+//     $('.josh').mouseover(function () {
+//         $('.mug').show();
+//         $('.kim').show();
+//     })
 
-    $('.josh').mouseout(function () {
-         $('.mug').hide();
-         $('.kim').hide();
-    })
-});
+//     $('.josh').mouseout(function () {
+//         $('.mug').hide();
+//         $('.kim').hide();
+//     })
+// });
 
-$(document).ready(function () {
-    $('.sharo').mouseover(function () {
-         $('.rec').show();
-         $('.bord').show();
-    })
+// $(document).ready(function () {
+//     $('.sharo').mouseover(function () {
+//         $('.rec').show();
+//         $('.bord').show();
+//     })
 
-    $('.sharo').mouseout(function () {
-         $('.rec').hide();
-         $('.bord').hide();
-    })
-});
-$(document).ready(function () {
-    $('.bra').mouseover(function () {
-         $('.kayo').show();
-         $('.border').show();
-    })
+//     $('.sharo').mouseout(function () {
+//         $('.rec').hide();
+//         $('.bord').hide();
+//     })
+// });
+// $(document).ready(function () {
+//     $('.bra').mouseover(function () {
+//         $('.kayo').show();
+//         $('.border').show();
+//     })
 
-    $('.bra').mouseout(function () {
-         $('.kayo').hide();
-         $('.border').hide();
-    })
-});
+//     $('.bra').mouseout(function () {
+//         $('.kayo').hide();
+//         $('.border').hide();
+//     })
+// });
 
 
 
-function Pizza(crust,size,toppings=[]){
-    this.crust=crust
-    this.size=size
-    this.toppings=toppings
+function Pizza(crust, size, toppings = []) {
+    this.crust = crust;
+    this.size = size;
+    this.toppings = toppings;
 }
-Pizza.prototype.myPizza=function(){
- var pricesize;
- var pricecruster
- if(this.size=="small"){
-     pricesize=500;
- }
-else if(this.size=="madium"){
-    pricesize=1000;
-}
-else {
-    pricesize=2000;
-}
-if(crust=="cripys"){
-    pricecrust=450;
-
-}
-if(crust=="glury"){
-    pricecruster=5000
-}
-else{
-    pricecruster=200
-}
-
-var priceToppings=[]
-var priceMashrooms=290;
-var priceonions=560;
-var priceolives=430;
-var y;
-for(y=0;y<this.toppings.length;y++){
-    if (this.toppings[y]=="onions"){
-  priceToppings.push(priceonions)
+Pizza.prototype.myPizza = function () {
+    var pricesize;
+    var pricecruster
+    if (this.size == "small") {
+        pricesize = 500;
+        debugger;
+    }
+    else if (this.size == "medium") {
+        pricesize = 1000;
+    }
+    else {
+        pricesize = 2000;
+    }
+    if (this.crust == "cripys") {
+        pricecrust = 450;
 
     }
-
-}
-for (y=0;y<this.toppings.length;y++){
-    if(this.toppings[y]=="mushrooms"){
-        priceToppings.push(priceMashrooms)
+    if (this.crust == "glury") {
+        pricecrust = 5000;
     }
-   
-}
-for(y=0;y<this.toppings.length;y++){
-    if(this.toppings[y]=="olives"){
-        priceToppings.push(priceolives);
+    else {
+        pricecrust = 200;
     }
-}
-var totalToppings=0
-var i=0
-while(i<priceToppings.length){
-    totalToppings+=priceToppings[i];
-    i++;
+
+    var priceToppings = [];
+    var priceMashrooms = 290;
+    var priceonions = 560;
+    var priceolives = 430;
+    var y;
+    for (y = 0; y < this.toppings.length; y++) {
+        if (this.toppings[y] == "onions") {
+            priceToppings.push(priceonions);
+
+        }
+
+    }
+    for (y = 0; y < this.toppings.length; y++) {
+        if (this.toppings[y] == "mushrooms") {
+            priceToppings.push(priceMashrooms);
+        }
+        console.log(priceMashrooms + " " + "helo");
+
+    }
+    for (y = 0; y < this.toppings.length; y++) {
+        if (this.toppings[y] == "olives") {
+            priceToppings.push(priceolives);
+        }
+
+    }
+    var totalToppings = 0;
+    var i = 0;
+    while (i < priceToppings.length) {
+        totalToppings += priceToppings[i];
+        i++;
+    }
+
+    var priceTotal = pricesize + pricecrust + totalToppings;
+    return priceTotal;
+  
 }
 
-var priceTotal=pricesize+pricecrust+totalToppings
-return priceTotal
 
-}
+$("form").submit(function (event) {
+    event.preventDefault();
+    var size = $("select#contr").val();
+    var crust = $("select#sel").val();
+    var tops = [];
+    var number = $("input#num").val();
+    tops = $("input:checkbox:checked").map(function () {
+        return $(this).val();
+    }).get();
+    var newpiz = new Pizza(size, crust, tops);
+    $("#total").text(newpiz.myPizza());
+    $("#display").text(newpiz.myPizza() * number);
+  
+});
 
 
 
